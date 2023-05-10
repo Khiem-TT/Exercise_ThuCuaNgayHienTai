87ca05adcd2806d4cfb61fa7f34855a12ade2856
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-05-10 10:55:03 +0700

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 let weekday = ["chủ nhật", "thứ hai", "thứ ba", "thứ tư", "thứ năm", "thứ sáu", "thứ bảy"];
-let today = new Date();
-let index = today.getDay();
-console.log(`Hôm nay là ${weekday[index]}`);
+let today = new Date().getDay();
+console.log(`Hôm nay là ${weekday[today]}`);
